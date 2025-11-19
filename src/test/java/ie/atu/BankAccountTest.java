8667dc98ce0832d50705817f21a968e7e226c5c5
Franklin Esedebe <G00465105@atu.ie>
2025-11-19 16:40:14 +0000

    Set up BankAccountTest

---
+++ src/test/java/ie/atu/BankAccountTest.java
@@ -0,0 +1,35 @@
+package ie.atu;
+
+import ie.atu.week9.BankAccount;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class BankAccountTest {
+
+    BankAccount account;
+
+    @BeforeEach
+    void setUp() {
+        account = new BankAccount();
+    }
+
+    void constructorInitialisation()
+    {
+        account = new BankAccount("ACC12345", "Paul", 100);
+        assertEquals("ACC12345", account.getAccNo());
+        assertEquals("Paul", account.getName());
+        assertEquals(100, account.getBalance());
+    }
+
+    @Test
+    void constructorNegativeInitialisation()
+    {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> new BankAccount("ACC12345", "Paul", 100));
+        assertEquals("Balance must be greater than 0.", ex.getMessage());
+    }
+
+}
